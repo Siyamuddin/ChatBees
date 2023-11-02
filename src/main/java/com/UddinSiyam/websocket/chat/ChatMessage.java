038b3a06324b88e4cd8efc44e1a27fb7eb51b6df
Siyam Uddin <73014136+Siyamuddin@users.noreply.github.com>
2023-11-02 19:53:53 +0900

    chat app initial commit

---
+++ src/main/java/com/UddinSiyam/websocket/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.UddinSiyam.websocket.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ src/main/java/com/UddinSiyam/websocket/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.UddinSiyam.websocket.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
