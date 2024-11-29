5c98e254e0fdc45727f05c6e13c4ba90e2222ecf
uddinsiyam <siyamuddin177@gmail.com>
2024-11-29 16:05:38 +0900

    prod ready

--- src/main/java/com/UddinSiyam/websocket/config/WebSocketConfig.java
+++ src/main/java/com/UddinSiyam/websocket/config/WebSocketConfig.java
@@ -14,8 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS().setWebSocketEnabled(true);
+                .withSockJS();
     }
 
     @Override
